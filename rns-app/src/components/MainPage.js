caacee98169f1f4ee9cd8f2b7122b195f9c7510c
David Mizativan-Ortega <dortega5185@gmail.com>
2020-06-08 09:51:44 -0500

    finished the mainpage almost

---
+++ rns-app/src/components/MainPage.js
@@ -0,0 +1,104 @@
+import React from 'react'
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1>RNS and its Attempt to Help with Seizures</h1>
+
+      <p>
+        When a seizure occurs, there are bursts of electrical activity in your
+        brain. This activity causes different symptoms depending on the type of
+        seizure and what part of the brain is involved. Seizures can take on
+        many different forms, affecting people in different ways. Anything that
+        your brain is capable of, applies the same during a seizure. For
+        example, your brain helps you move, see, feel and do many other things.
+        Those same functions fully apply during a seizure as well, voluntarily
+        or involuntarily. Also, in some seizures, parts of the brain can still
+        function normally while others can’t. Seizures have a beginning, middle,
+        and end, but sometimes not all the parts of a seizure are visible or
+        easy to tell apart. It’s possible you won’t have every stage or symptom,
+        but if you have more than one seizure, you may notice that your seizures
+        are very similar or can repeat in a similar fashion. Seizures are
+        complex, they come and go, some you’re possibly able to anticipate while
+        others are sudden.
+      </p>
+
+      <p>
+        Those that can be anticipated allow for the person having a seizure
+        better prepare so nothing dangerous occurs, an early sign of a seizure
+        is called a prodrome. These feelings are usually not part of the
+        seizure, but they serve as a warning sign that a seizure may come, if
+        you were a person that has these signs you can take steps to stay safe
+        and position yourself in a safer area. Some people have an “aura,” or
+        warning. This is the first symptom of a seizure. It’s considered part of
+        the seizure, since bursts of electrical activity have already initiated
+        in the brain when this occurs. During this part of the seizure, you are
+        still conscious and aware of what is going on. An aura is often a change
+        in feeling, sensation, thought, or behavior. If you have more than one
+        seizure, you may have a similar aura each time. Some don't experience or
+        sense the aura or warning. When they have a seizure, they lose
+        consciousness or awareness immediately. Typically, an aura happens
+        before the main part of a seizure. The aura can occur as an isolated
+        incident, without any other symptoms. Those are defined as a simple
+        partial seizure.{' '}
+      </p>
+
+      <p>
+        A generalized tonic-clonic seizure, sometimes called a grand mal
+        seizure, is a disturbance in the functioning of both sides of your
+        brain. This disturbance is caused by electrical signals spreading
+        through the brain inappropriately. Often this will result in signals
+        being sent to your muscles, nerves, or glands. The spread of these
+        signals in your brain can make you lose consciousness and have severe
+        muscle contractions.{' '}
+      </p>
+
+      <h2>Common symptoms of "Aurul Phase"</h2>
+
+      <h5>Changes in your Thoughs and Feelings:</h5>
+
+      <ol>
+        <li>Strange, negative, or scary feelings, like fear or panic</li>
+        <li>Pleasant feelings</li>
+        <li>Racing thoughts</li>
+        <li>
+          Déjà vu (a feeling that you have been somewhere before when you
+          haven’t)
+        </li>
+        <li>
+          Jamais vu (a feeling that something is unfamiliar even though it
+          isn’t)
+        </li>
+        <li>Thinking that your body parts feel or look different</li>
+        <li>Out of body sensation</li>
+
+        <h6>Changes in your Body:</h6>
+
+        <li>Fluctuating body temperature (Hot, cold, sweaty) </li>
+        <li>Looking pale or flushed</li>
+        <li>Heart racing</li>
+        <li>Feeling dizzy or lightheaded</li>
+        <li>Nausea (feeling like you might throw up) </li>
+        <li>
+          Other feelings in your stomach (like a rising feeling from your
+          stomach to your throat){' '}
+        </li>
+        <h6>Changes in your Senses</h6>
+
+        <li>Headache or pain in the body</li>
+        <li>
+          Numbness, tingling, or shock/spasm in a random part of your body
+        </li>
+        <li>Unusual smells, sounds, or tastes</li>
+        <li>Vision loss, blurry vision</li>
+        <li>Seeing flashing lights or things that are not really there</li>
+        <li>Hearing loss or temporary deafness</li>
+        <li>Hearing strange or different sounds</li>
+        <li>
+          Unusual tastes or smells (for example, a bad smell like burning
+          rubber)
+        </li>
+      </ol>
+    </div>
+  )
+}
